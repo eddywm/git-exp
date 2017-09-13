9d65bae064ebc454b2febdeffcd3ad073d17ab25
Eddy WM <eddywmdev@gmail.com>
2017-09-13 19:49:12 +0300

    x -> z && square (5)

--- src/my_square.py
+++ src/my_square.py
@@ -1,7 +1,7 @@
 
-def my_square(x) :
+def my_square(z) :
         # The function takes a value and returns its square value
-        # All that by using the ** operator
-        return(x ** 2)
+        # All that by using the * operator
+        return(z * z)
 
-print(my_square(10))
+print(my_square(5))
