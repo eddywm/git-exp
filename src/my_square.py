5d5825b2625b115c5eed240d6d5130196c20b0c9
eddywm <eddywmdev@gmail.com>
2017-09-13 19:43:02 +0300

    x -> y && square (25)

--- src/my_square.py
+++ src/my_square.py
@@ -1,7 +1,7 @@
 
-def my_square(x) :
+def my_square(y) :
         # The function takes a value and returns its square value
         # All that by using the ** operator
-        return(x ** 2)
+        return(y ** 2)
 
-print(my_square(10))
+print(my_square(25))
